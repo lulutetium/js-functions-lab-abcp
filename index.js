9f4bb615e6e56f17865f6566c064321716f33639
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-13 04:39:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function happyHolidaysTo(name) {
   return "Happy holidays ${name}!";
 }
 
-happyHolidaysTo('you');
 
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!";
